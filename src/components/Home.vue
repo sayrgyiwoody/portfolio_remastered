5fdafdf20566f3762f398012ee698f0899d94048
Woody <waiyanwoody@gmail.com>
2023-10-29 15:52:08 +0630

    CV form added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,10 +12,10 @@
             <h5 v-scroll-reveal.reset="{origin : 'top' , distance : '20px',delay: 600}" class="text-primary text-[28px]">Wai Yan Tun</h5>
             <p  v-scroll-reveal.reset="{origin : 'top' , distance : '30px',delay: 700}" class="text-gray-500 dark:text-muted">Welcome to my portfolio! Explore my work and skills in web development and design. Let's create something amazing together!</p>
             <div v-scroll-reveal.reset="{origin : 'top' , distance : '20px',delay: 900}">
-                <button  class="flex justify-center items-center bg-primary hover:bg-primary_hover duration-150 px-4 py-2 text-white rounded-md mt-4">
+                <a target="_blank" download="Waiyan Tun CV Form" :href="`./WaiyanTun_CV.pdf`"  class="flex justify-center items-center bg-primary hover:bg-primary_hover duration-150 px-4 py-2 text-white rounded-md mt-4 w-fit">
                     <svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="currentColor"><path fill-rule="evenodd" d="M12 22c-1.886 0-2.828 0-3.414-.586C8 20.828 8 19.886 8 18c0-1.886 0-2.828.586-3.414C9.172 14 10.114 14 12 14c1.886 0 2.828 0 3.414.586C16 15.172 16 16.114 16 18c0 1.886 0 2.828-.586 3.414C14.828 22 13.886 22 12 22Zm1.805-3.084l-1.334 1.333a.667.667 0 0 1-.942 0l-1.334-1.333a.667.667 0 1 1 .943-.943l.195.195v-1.946a.667.667 0 0 1 1.334 0v1.946l.195-.195a.667.667 0 0 1 .943.943Z" clip-rule="evenodd"/><path d="M6.5 18v-.09c0-.865 0-1.659.087-2.304c.095-.711.32-1.463.938-2.08c.618-.619 1.37-.844 2.08-.94c.646-.086 1.44-.086 2.306-.086h.178c.866 0 1.66 0 2.305.087c.711.095 1.463.32 2.08.938c.619.618.844 1.37.94 2.08c.085.637.086 1.416.086 2.267c2.573-.55 4.5-2.812 4.5-5.52c0-2.47-1.607-4.572-3.845-5.337C17.837 4.194 15.415 2 12.476 2C9.32 2 6.762 4.528 6.762 7.647c0 .69.125 1.35.354 1.962a4.356 4.356 0 0 0-.83-.08C3.919 9.53 2 11.426 2 13.765C2 16.104 3.919 18 6.286 18H6.5Z"/></g></svg>
                     Download CV
-                </button>
+                </a>
             </div>
            
             <div v-scroll-reveal.reset="{origin : 'bottom' , distance : '30px',delay: 700}" class=" mt-4 md:mt-8">
